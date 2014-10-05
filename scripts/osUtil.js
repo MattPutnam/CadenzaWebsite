558fc7a48386f5001d0175b6177092e64abd8d6a
Matt Putnam <matt.s.putnam@gmail.com>
2014-10-05 00:29:22 -0400

    Initial commit

---
+++ scripts/osUtil.js
@@ -0,0 +1,27 @@
+osName = 'Unknown';
+
+function nav(x, y, z) {
+  z = z || y;
+  if (navigator[x] && navigator[x].indexOf(y) !== -1) {
+    osName = z;
+  }
+}
+
+/*   navigator     value     download  */
+nav( "appVersion", "X11",    "UNIX"    );
+nav( "appVersion", "Mac",    "MacOS"   );
+nav( "appVersion", "Linux"             );
+nav( "userAgent",  "Linux"             );
+nav( "platform",   "Linux"             );
+nav( "appVersion", "Win",    "Windows" );
+nav( "userAgent",  "Windows"           );
+nav( "platform",   "Win",    "Windows" );
+nav( "oscpu",      "Windows"           );
+
+function getOSName() {
+  return osName;
+}
+
+function isMacOS() {
+  return osName == "MacOS";
+}
